04760b99d31c7189c2a50ae52d31eebea52ad199
fastdid <ddavalos85@hotmail.com>
2025-02-16 00:11:47 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import FileSplitter from "/src/components/FileSplitter";
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Gestor de División de Archivos</h1>
+            <FileSplitter />
+        </div>
+    );
+}
+
+export default App;
